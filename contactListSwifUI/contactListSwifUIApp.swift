4b4b9a7a1c64ee5610f3409d742ad2961e69ebc6
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-03-02 23:08:00 +0200

    Initial Commit

---
+++ contactListSwifUI/contactListSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  contactListSwifUIApp.swift
+//  contactListSwifUI
+//
+//  Created by BSergio on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct contactListSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
